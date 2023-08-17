3dbdcec74d5787b73146ab13486183cd1aefe5a1
conicuznhm <conicuzn@hotmail.com>
2023-08-17 10:45:06 +0700

    set some dir

--- controllers/userController.go
+++ controllers/userController.go
@@ -12,7 +12,8 @@ func GetProfile(c echo.Context) error{
 	idStr := c.Param("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil{
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{"error":"Invalid id"})
+		// return c.JSON(http.StatusBadRequest, map[string]interface{}{"error":"Invalid id"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"error":"Invalid id"})
 	}
 
 	profile, err := services.FetchProfile(id)
--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"RCSserver/routes"
 
+	// "fmt"
+	// "log"
+
+	// dbs "github.com/conicuznhm/rcsdb/dbservice" //To create database
+	// sfn "github.com/conicuznhm/rcsdb/dbservice/servicefn" //To use path and id fn
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -17,4 +22,18 @@ func main(){
 	
 	port := ":8888"
 	e.Logger.Fatal(e.Start(port))
+	
+	// to create database
+	// if err := dbs.JSONdbCreate("","user"); err != nil{
+	// 	fmt.Println(err)
+	// 	log.Fatal(err)
+	// }
+
+	// to read path
+	// if path, err := sfn.GetPath("user"); err != nil{
+	// 	fmt.Printf("%v\n",err)
+	// 	log.Fatal(err)
+	// }else{
+	// 	fmt.Println(path.Lastid)
+	// }
 }
